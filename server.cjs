f2b650b62a71692974fd994ebf7c4e649349234e
= <Py_nax0@gmail.com>
2022-09-09 17:55:40 +0900

    some changes

--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 1234;
+const PORT = process.env.PORT || 5050;
 
 app.use(express.static('src'));
 
@@ -9,5 +9,5 @@ app.get("/", (req,res)=>{
 })
 
 app.listen(PORT,()=>{
-    console.log(`Listening in localhost:${PORT}`);
+    console.log(`Listening in PORT ${PORT}`);
 })
